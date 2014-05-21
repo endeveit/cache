466856dc5c120eab5b8b5c0d7fdcfb1af1040cb7
Igor Borodikhin <iborodikhin@gmail.com>
2014-05-21 14:50:19 +0600

    Correct @author tag

--- src/Endeveit/Cache/Drivers/Memcached.php
+++ src/Endeveit/Cache/Drivers/Memcached.php
@@ -3,7 +3,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Nikita Vershinin <endeveit@gmail.com>
+ * @author Igor Borodikhin <iborodikhin@gmail.com>
  * @license MIT
  */
 namespace Endeveit\Cache\Drivers;
