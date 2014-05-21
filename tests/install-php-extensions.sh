65e8baf000708e2dab5808615526f5085b772a99
Igor Borodikhin <iborodikhin@gmail.com>
2014-05-21 15:13:29 +0600

    Added dependencies for tests

--- tests/install-php-extensions.sh
+++ tests/install-php-extensions.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-printf "no\n" | pecl install memcache > /dev/null
-pecl install mongo > /dev/null
-pecl install redis > /dev/null
+printf "no\n" | pecl install memcache
+pecl install memcached
+pecl install mongo
+pecl install redis
 
 echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
