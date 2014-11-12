407aa7be1032eb95b848e5eea361caabf238d19f
Nikita Vershinin <endeveit@gmail.com>
2014-11-12 17:04:29 +0600

    Trying to fix build errors

--- tests/enable-php-extensions.sh
+++ tests/enable-php-extensions.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+DIR=$(readlink -enq $(dirname $0))
+
+sudo apt-get -qq update &
+
+# Enable APC
+if [ "$(php -r 'echo PHP_VERSION_ID;')" -ge 50500 ]; then
+    ( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
+else
+    ( CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl upgrade apc < /dev/null; phpenv config-add "$DIR/apc.ini" ) &
+fi
+
 echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
-# Enable APC
-echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
