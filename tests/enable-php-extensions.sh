6d8e918349e3181e4443c144391ddfde32a7460d
Nikita Vershinin <endeveit@gmail.com>
2014-11-13 16:13:18 +0600

    Continue with Travis CI

--- tests/enable-php-extensions.sh
+++ tests/enable-php-extensions.sh
@@ -6,17 +6,10 @@ sudo apt-get -qq update &
 
 # Enable custom APC
 if [ "$(php -r 'echo PHP_VERSION_ID;')" -ge 50500 ]; then
-    ( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
+    ( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/z-apcu.ini" ) &
 else
-    ( CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl upgrade apc < /dev/null; phpenv config-add "$DIR/apc.ini" ) &
+    ( CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl upgrade apc < /dev/null; phpenv config-add "$DIR/z-apc.ini" ) &
 fi
 
-# Build XCache
-curl -sS -o xcache-3.2.0.tar.gz http://xcache.lighttpd.net/pub/Releases/3.2.0/xcache-3.2.0.tar.gz
-tar -xzf xcache-3.2.0.tar.gz
-sh -c "cd xcache-3.2.0 && phpize && ./configure && make && sudo make install && cd .."
-rm -rf xcache-3.2.0
-rm xcache-3.2.0.tar.gz
-
 # Enable extensions
-phpenv config-add "$DIR/travis.ini"
+phpenv config-add "$DIR/z-travis.ini"
